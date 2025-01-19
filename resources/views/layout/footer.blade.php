e273536e3eff659546b201d3adf5b081b99daf8d
Tanya8715 <atanya22@tbc.edu.np>
2025-01-19 15:33:27 +0545

    Added a map

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,16 @@
 <footer class="footer py-4">
-    <div class="container-fluid">
-      <div class="d-flex justify-content-end">
-      </div>
+  <div class="container-fluid">
+    <div class="d-flex justify-content-end">
+      <!-- Google Map Embed -->
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.698736007761!2d85.32401971507334!3d27.717245982800794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19342b098063%3A0x4b19d63d4f1a6c39!2sKathmandu!5e0!3m2!1sen!2snp!4v1674398254517!5m2!1sen!2snp"
+        width="600"
+        height="450"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+      </iframe>
     </div>
-  </footer>
+  </div>
+</footer>
